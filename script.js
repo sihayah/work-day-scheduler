37113cc6bcdc03f0e99e39b2a4f1a7b8e8882f54
Sihayah <sihaya_a@hotmail.com>
2021-11-14 12:36:38 -0500

    save localstor

--- script.js
+++ script.js
@@ -8,53 +8,77 @@ new Array(9).fill().map((acc, index) => {
 
 // generate heading stating date and time at bottom of header
 
-var today = moment().format("[Today is] dddd h:mmA"); ;
+var today = moment().format("[Today is] dddd h:mmA");;
 $("#currentDay").text(today);
 // generate rows
 
-var hourRow = $("<div>").addClass("row");
+
 
 // set this moment equal to var now
 var now = moment().format('HHA');
 
+
 var userEvents = [];
+
+if (localStorage.getItem('events')){
+    userEvents = JSON.parse(localStorage.getItem('events'));
+}
 
 // loop through the hours of the day and append a div for each with the time, space to add events, and a button to save events
 
-for (var i=0; i<hours.length; i++){
+for (var i = 0; i < hours.length; i++) {
     var hourI = hours[i];
     var currentHour = moment(hourI, 'HHA').format('hA')
     var time = hours[i];
     var text = $(this)
-    .text()
-    .trim();
+        .text()
+        .trim();
+    
+        var hourRow = $("<div>").addClass("row");
     var hourBox = $("<div>").addClass("col-2  border-left-0 hour-box d-flex justify-content-center align-items-center hour block").text(currentHour);
 
     var textArea = $("<textarea>")
-    .addClass("col-8 d-flex justify-content-center align-items-center text-area").attr("type", "text").val("");
+        .addClass("col-8 d-flex justify-content-center align-items-center textarea time-block").attr("type", "text").attr("id", currentHour);
 
+    var saveButton = $("<button>")
+        .addClass("col-2 rounded-right saveBtn d-flex justify-content-center align-items-center").attr("id", hours[i]);
 
-    var saveButton = $("<div>")
-    .addClass("col-2 rounded-right saveBtn d-flex justify-content-center align-items-center");
-    
     $(".container").append(hourRow)
     $(hourRow).append(hourBox);
     $(hourRow).append(textArea);
-
     $(hourRow).append(saveButton);
-    
+
     var iconSpan = $("<i>").addClass("fa fa-check-circle");
     $(saveButton).append(iconSpan);
 
-    $(saveButton).click(function() {
-    })    
-
     // color code the text area according to where we are in the day 
-    if (time>now) {
+    if (time > now) {
         $(textArea).addClass("future")
-    } else if (time<now) { 
-       $(textArea).addClass("past")
+    } else if (time < now) {
+        $(textArea).addClass("past")
     } else {
         $(textArea).addClass("present")
     }
 };
+
+
+
+
+
+
+$('button').on("click", function () {
+    var currentTimeblockTime = $(this).siblings('div').text();
+    var textAreaInput = $(this).siblings('textarea').val();
+    userEvents.push({
+        time: currentTimeblockTime,
+        message: textAreaInput
+    })
+    localStorage.setItem("events", JSON.stringify(userEvents))
+
+});
+
+
+for(var i = 0; i < userEvents.length; i++){
+    // console.log(`#${userEvents[i].time}`);
+    $(`#${userEvents[i].time}`).text(userEvents[i].message)
+}
